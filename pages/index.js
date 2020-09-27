4a3cabf121605b24a0c103ee65c0c135acd24894
Joseph Surla <josephsurla@gmail.com>
2020-09-27 14:24:14 +0900

    Update homepage styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -11,18 +13,21 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Thoughts and observations.</p>
+        <p className={utilStyles.textMd}>Thoughts and observations.</p>
+        <p className={utilStyles.textSm}>Writings by <a href="https://twitter.com/josephsurla">Joseph Surla</a></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+                  </small>
             </li>
           ))}
         </ul>
